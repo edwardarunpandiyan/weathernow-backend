199b8633175cccf298647c572f5e04c87647f0c5
edwardarunpandiyan <44399557+edwardarunpandiyan@users.noreply.github.com>
2026-01-14 08:49:48 +0000

    add feature daily

--- services/weather_service.py
+++ services/weather_service.py
@@ -9,10 +9,14 @@ def get_weather_by_coords(latitude, longitude):
         params={
             "latitude": latitude,
             "longitude": longitude,
-            "current_weather": True
+            "current_weather": True,
+            "hourly": "temperature_2m,weathercode,windspeed_10m",
+            "daily": "temperature_2m_max,temperature_2m_min,weathercode",
+            "forecast_days": "7",
+            "timezone": "auto"
         }
     )
-
+    print(response)
     if response.status_code != 200:
         return {"error": "Weather data not found"}
     data = response.json()
@@ -21,17 +25,32 @@ def get_weather_by_coords(latitude, longitude):
         return {"error": "Current weather data not available"}
 
     current = data["current_weather"]
+    daily_data = data["daily"] if data.get("daily") else {}
+    # daily_data = data["daily"]
+    daily = []
+    for i in range(len(daily_data["time"])):
+         daily.append(
+            {
+            "date": daily_data["time"][i],
+            "max_temperature": daily_data["temperature_2m_max"][i],
+            "min_temperature": daily_data["temperature_2m_min"][i],
+            "condition": WEATHER_CODE_MAP.get(daily_data["weathercode"][i], "Unknown")
+            }
+         )
 
     return {
         "data": {
-            "latitude": data["latitude"],
-            "longitude": data["longitude"],
-            "temperature": current["temperature"],
-            "condition": WEATHER_CODE_MAP.get(current["weathercode"], "Unknown"),
-            "windspeed": current["windspeed"],
-            "winddirection": current["winddirection"],
-            "time": current["time"],
-            "is_day": bool(current["is_day"]),
+            "current": {
+                "latitude": data["latitude"],
+                "longitude": data["longitude"],
+                "temperature": current["temperature"],
+                "condition": WEATHER_CODE_MAP.get(current["weathercode"], "Unknown"),
+                "windspeed": current["windspeed"],
+                "winddirection": current["winddirection"],
+                "time": current["time"],
+                "is_day": bool(current["is_day"]),
+            },
+            "daily": daily,
             "units": {
                 "temperature": data["current_weather_units"]["temperature"],
                 "windspeed": data["current_weather_units"]["windspeed"],
